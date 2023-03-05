e1ce03aa7c4eaa39879ccabddf05eccb9c596415
kingtuler <morozovsergey.03@gmail.com>
2023-03-06 00:45:46 +0400

    Финал 7го варианта

--- first.py
+++ first.py
@@ -1,20 +1,20 @@
 def main():
-    alfavit =  'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ. ,!'
-    key=        'ВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБ. ,!'
-    message=''
-    with open("Исходный_текст_1", mode="r", encoding='utf-8') as r_file:
-        message=r_file.read()
-    message=message.replace("\n",'')
-    itog = ''   
+    alfavit = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ. ,!"
+    key = "ВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБ. ,!"
+    message = ""
+    with open("Исходный_текст_1", mode="r", encoding="utf-8") as r_file:
+        message = r_file.read()
+    message = message.replace("\n", "")
+    itog = ""
     for i in message.upper():
-        tmp=alfavit.index(i)
-        itog+=key[tmp]
-    with open ("Ключ_к_тексту_1",mode='w+') as file:
-        file.write(str(dict(zip(alfavit,key))))
+        tmp = alfavit.index(i)
+        itog += key[tmp]
+    with open("Ключ_к_тексту_1", mode="w+") as file:
+        file.write(str(dict(zip(alfavit, key))))
 
-    with open("Зашифрованный_текст_1",mode='w+') as file:
+    with open("Зашифрованный_текст_1", mode="w+") as file:
         file.write(itog)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
